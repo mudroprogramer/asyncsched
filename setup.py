afe24ad29aec13a19f0c8e22d229afd61ea52714
Alexander Shapiro <mudroprogramer@gmail.com>
2022-08-04 23:09:27 -0700

    update setup

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setuptools.setup(
     author_email='mudroprogramer@gmail.com',
     description='Simple coroutine scheduler for asyncio',
     license='MIT',
-    packages=['asyncsched'],
+    packages=['asyncsched', 'asyncsched.src'],
     install_requires=['asyncio', 'datetime', 'typing', 'freezegun', 'pytest'],
 )
